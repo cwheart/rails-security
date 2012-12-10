f0e96f8ac54fb0bdf3aebd7281ab1641da1b6c23
xin <xinshuaifeng@126.com>
2012-12-10 18:37:21 +0800

    function

--- lib/xss.rb
+++ lib/xss.rb
@@ -1,22 +1,32 @@
-# -*- coding: undecided -*-
 module RailsSecurity
   module Xss
-    safe_tag_names = %w(span div p font)
-    reg = /<\/?span\s*[a-zA-Z0-9=_\-\s;]*\s*>/
-    def replace_tag
-      
+    SAFE_TAGS = %w(span div p font)
+    TAG_BEG = "[rs:beg]"
+    TAG_END = "[rs:end]"
+    
+    def html_safe(str)
+      str = replace_keyword(str)
+      str = replace_tag(str)
+      str = replace_dirty(str)
+      str = revert_tag(str)
     end
-
-    def replace_keywork(str)
-      str.gsub(/¡Ü/, "&le;").gsub(/¡Ý/, '&ge;');
+    module_function :html_safe
+    
+    private
+    def replace_keyword(str)
+      str
     end
-
+    def replace_tag(str)
+      str.gsub(instance_safe_reg, "#{TAG_BEG}\\1#{TAG_END}")
+    end
     def replace_dirty(str)
       str.gsub(/</, "&lt;").gsub(/>/, "&gt;")
     end
-
-    def revert_tag
-      
+    def revert_tag(str)
+      str.gsub(/\[rs:beg\]/, "<").gsub(/\[rs:end\]/, ">")
+    end
+    def instance_safe_reg
+      Regexp.compile("<(\/?(?:#{SAFE_TAGS.join('|')})(?:\\s+[a-z0-9\\s_=\\-\\'"+'\"'+"]+)?)>")
     end
   end
 end
